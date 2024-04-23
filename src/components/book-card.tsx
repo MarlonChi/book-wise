4463cb4e5e049949cb875fd5896588404d9bdff5
MarlonChi <marlonchiodelli@hotmail.com>
2024-04-23 19:08:36 -0300

    feat: book card

---
+++ src/components/book-card.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+export const BookCard = () => {
+  return (
+    <div className="bg-gray-700 rounded-lg p-6 flex gap-5">
+      <div className="rounded-md">
+        <Image
+          src="/images/books/Book.png"
+          alt=""
+          width={64}
+          height={94}
+          className="w-16 h-24"
+        />
+      </div>
+      <div className="flex flex-1 flex-col justify-between">
+        <div>
+          <h3 className="font-bold">O Hobbit</h3>
+          <p className="text-gray-400">J.R.R. Tolkien</p>
+        </div>
+
+        <div>Rating stars</div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/rating-card-book.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+export const RatingCardBook = () => {
+  return (
+    <div className="bg-gray-700 rounded-lg p-6">
+      <header className="flex justify-between">
+        <div className="flex items-center gap-4">
+          <div className="rounded-full w-10 h-10 ">
+            <Image
+              src="https://github.com/MarlonChi.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+          </div>
+          <div>
+            <p>Marlon Chiodelli</p>
+            <p className="text-gray-400">Hoje</p>
+          </div>
+        </div>
+        <div>Rating stars</div>
+      </header>
+
+      <div className="mt-8 flex gap-5">
+        <div className="rounded-md">
+          <Image
+            src="/images/books/o-hobbit.png"
+            alt=""
+            width={108}
+            height={152}
+          />
+        </div>
+        <div className="flex flex-1 flex-col">
+          <h3 className="font-bold">O Hobbit</h3>
+          <p className="text-gray-400">J.R.R. Tolkien</p>
+
+          <p className="text-gray-300">
+            Semper et sapien proin vitae nisi. Feugiat neque integer donec et
+            aenean posuere amet ultrices. Cras fermentum id pulvinar varius leo
+            a in. Amet libero pharetra nunc elementum fringilla velit ipsum. Sed
+            vulputate massa velit nibh...
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
